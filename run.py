d2fd7cd12e417195378e271dad3d8ee96ba445d3
ShaoLeei <QWERTY0202sl>
2019-07-15 21:04:39 +0800

    获取页面_有Bug_乱码;

---
+++ run.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: Hamdi
+import urllib2
+import re
+
+
+class Spider:
+    """
+    内涵段子爬虫类
+    """
+    def __init__(self):
+        """
+        初始化
+        """
+        self.page = 1
+
+    def loadPage(self, page):
+        """
+        下载页面
+        :return:
+        """
+
+        print '正在下载数据……'
+        url = "https://www.neihan8s.com/article/list_5_" + str(page) + ".html"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
+        }
+        # 请求页面
+        request = urllib2.Request(url, headers=headers)
+        # 打开页面
+        response = urllib2.urlopen(request)
+        # 读取页面, 返回给html
+        html = response.read()
+        print html
+
+
+if __name__ == '__main__':
+    print '请按下回车开始'
+    raw_input()
+    # 定义一个Spider对象
+    spider = Spider()
+    spider.loadPage(1)
