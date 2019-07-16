95529c328d55330f2672bc0980effe08229b3391
ShaoLeei <javs_shao@163.com>
2019-07-16 09:56:17 +0800

    使用re清洗数据

--- run.py
+++ run.py
@@ -10,11 +10,7 @@ class Spider:
     内涵段子爬虫类
     """
     def __init__(self):
-        """
-        初始化
-        """
         self.page = 1
-
     def loadPage(self, page):
         """
         下载页面
@@ -22,7 +18,6 @@ class Spider:
 
         """
 
-        print '正在下载数据……'
         url = "https://www.neihan8s.com/article/list_5_" + str(page) + ".html"
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36"
@@ -33,9 +28,35 @@ class Spider:
         response = urllib2.urlopen(request)
         # 读取页面, 返回给html
         html = response.read()
-        html = html.decode('gbk').encode('utf-8')
-        print html
+        gbk_html = html.decode('gbk').encode('utf-8')
 
+        pattern = re.compile('<div\sclass="f18 mb20">(.*?)</div>', re.S)
+        content_list = pattern.findall(gbk_html)
+
+        # return content_list
+        self.printOnePage(content_list)
+
+
+    def printOnePage(self, content_list):
+        """
+            @brief 处理得到的段子列表
+            @param item_list 得到的段子列表
+            @param page 处理第几页
+        """
+
+        for item in content_list:
+            item = item.replace("<p>", "").replace("</p>", "").replace("<br />", "")
+            self.writePage(item)
+
+    def writePage(self, item):
+        """
+        写入数据
+        :param item:
+        :return:
+        """
+        print "正在写入数据....."
+        with open("duanzi.txt", "a") as f:
+            f.write(item)
 
 if __name__ == '__main__':
 
